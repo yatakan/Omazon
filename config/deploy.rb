f1189a51b095e3f527977c0bfdadefec9ca2969a
yatakan <takaya@fuzaketanamaetsukeruna.local>
2017-07-12 19:53:45 +0900

    capistrano
    訂正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
-lock '<Capistranoのバージョン>'
+lock '3.8.2'
 
-set :application, '自身のアプリケーション名'
-set :repo_url,  'git@github.com:<Githubのユーザー名>/<レポジトリ名>.git'
+set :application, 'Omazon'
+set :repo_url,  'git@github.com:takaya423@outlook.jp/https://github.com/yatakan/Omazon.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -9,7 +9,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['<ローカルPCのEC2インスタンスのSSH鍵(pem)へのパス>']  ※例：/Users/yusukeyamane/.ssh/key_pem.pem
+                  keys: ['/Users/takaya/.ssh/loxonin.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
