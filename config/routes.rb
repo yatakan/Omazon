cf5e324dea918a24afa828caaea3cdfbb006430a
yatakan <takaya@fuzaketanamaetsukeruna.local>
2017-07-13 21:34:45 +0900

    さて帰ろう
    コントローラ作ってルーティングの設定まで終わり
    明日検索機能を作れたらいいな

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
+  get 'items/search' => 'items#search'
   resources :categories,only: [:index]
   resources :items, only: [:index] do
     resources :reviews, only: [:index]
