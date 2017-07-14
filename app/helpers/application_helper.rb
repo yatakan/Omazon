5464500c932b17d092f1d27e22ba8d6733194a98
yatakan <takaya@fuzaketanamaetsukeruna.local>
2017-07-14 17:11:46 +0900

    ユーザー管理機能
    とりあえず登録できるようにしました。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_controll
+    if user_signed_in?
+      current_user.name
+    else
+      link_to "サインイン", new_user_session_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'items#index'
   get 'items/search' => 'items#search'
   resources :categories,only: [:index]
