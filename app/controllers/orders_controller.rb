6671fe099f1b8efcde20478d1b9141e33aaeb821
yatakan <takaya@fuzaketanamaetsukeruna.local>
2017-07-18 16:11:21 +0900

    Revert "注文機能"
    This reverts commit 8b70e6ba24d4bdff1e4073ec04a110a598628e4d.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,19 +12,9 @@ class OrdersController < ApplicationController
   end
 
   def edit
-    @order = Order.find(params[:id])
-    @carts = Cart.where(user_id: current_user.id)
-    if @carts.present?
-      @sum = 0
-      @carts.each do |cart|
-        @sum += (cart.item.price * cart.number)
-      end
-    end
   end
 
   def update
-    @order = Order.find(params[:id])
-    @order.update(order_params)
   end
 
   private
