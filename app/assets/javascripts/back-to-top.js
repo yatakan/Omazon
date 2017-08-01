b01d7d4b19baa3741aed9f6cedf88b40e58b1942
yatakan <takaya@fuzaketanamaetsukeruna.local>
2017-08-01 11:52:36 +0900

    修正
    修正

--- app/assets/javascripts/back-to-top.js
+++ app/assets/javascripts/back-to-top.js
@@ -1,7 +1,6 @@
 $(document).on('turbolinks:load',function() {
   $("#back-to-top").on('click',function() {
     $('body, html').animate( { scrollTop: 0 }, 500);
-    $('.unko').append('<img src="assets/book1.jpg">');
     return false;
   });
 });
